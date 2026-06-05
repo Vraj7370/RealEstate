df03ae8acefd66d04741b0695cb79fd1576985ac
Vraj patel <patelvraj7372@gmail.com>
2026-06-05 18:05:24 +0530

    deploy

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001/api';
+const BASE_URL = process.env.REACT_APP_API_URL || 'https://realestatee-pzqf.onrender.com/api';
 
 const API = axios.create({
   baseURL: BASE_URL,
